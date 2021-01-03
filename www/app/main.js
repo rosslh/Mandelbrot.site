35aa5c122e1c42649ee237bff423b2467619c0c3
Ross Hill <ross@rosshill.ca>
2021-01-02 23:16:00 -0500

    add num workers control

--- www/app/main.js
+++ www/app/main.js
@@ -14,7 +14,8 @@ import debounce from "debounce";
 
 let maxIterations = 200;
 let isSmoothed = true;
-const numWorkers = Math.min(navigator.hardwareConcurrency || 6, 10);
+const numCores = navigator.hardwareConcurrency || 6;
+let numWorkers = numCores;
 let workers = [];
 
 function createWorker() {
@@ -25,8 +26,8 @@ function createWorker() {
   };
   const workerReadyHandler = e => {
     if (e.data.ready) {
-      w.worker.removeEventListener("message", workerReadyHandler); // collect garbage
       w.ready = true;
+      w.worker.removeEventListener("message", workerReadyHandler);
     }
   };
   w.worker.addEventListener("message", workerReadyHandler);
@@ -49,17 +50,17 @@ function createTile(coords, done) {
   const coordsString = JSON.stringify(coords);
   const selectedWorker = workers.filter(w => w.ready).sort((a, b) => (a.activeJobs.length > b.activeJobs.length) ? 1 : -1)[0];
   if (!selectedWorker) {
-    alert("Sorry! Something went wrong. Try refreshing your page.");
+    alert("Sorry! Something went wrong. Try refreshing the page.");
     return tile;
   }
   selectedWorker.activeJobs.push(coordsString);
   const tileRetrievedHandler = e => {
     if (e.data.coords === coordsString) {
-      selectedWorker.worker.removeEventListener("message", tileRetrievedHandler); // collect garbage
-      selectedWorker.activeJobs = selectedWorker.activeJobs.filter(j => j !== coordsString);
       const imageData = new ImageData(Uint8ClampedArray.from(e.data.pixels), 256, 256);
       ctx.putImageData(imageData, 0, 0);
       done(undefined, tile);
+      selectedWorker.worker.removeEventListener("message", tileRetrievedHandler);
+      selectedWorker.activeJobs = selectedWorker.activeJobs.filter(j => j !== coordsString);
     }
   };
   selectedWorker.worker.addEventListener("message", tileRetrievedHandler);
@@ -70,14 +71,16 @@ function createTile(coords, done) {
 function createMap() {
   const tiles = new L.GridLayer({ tileSize: 256 });
   tiles.createTile = createTile;
-  const options = { attributionControl: false, noWrap: true, maxZoom: 32, zoomAnimationThreshold: 1000, scrollWheelZoom: true };
+  const options = { attributionControl: false, maxZoom: 32, zoomAnimationThreshold: 32 };
   const map = L.map('leaflet-map', options).setView([0, 0], 2);
   tiles.addTo(map);
   return map;
 }
 
 function refreshMap(map) {
-  map._resetView(map.getCenter(), map.getZoom(), true);
+  resetWorkers().then(() => {
+    map._resetView(map.getCenter(), map.getZoom(), true);
+  });
 };
 
 function handleInputs(map) {
@@ -90,18 +93,26 @@ function handleInputs(map) {
     }
     iterationsInput.value = parsedValue;
     maxIterations = parsedValue;
-    resetWorkers().then(() => {
-      refreshMap(map);
-    })
+    refreshMap(map);
+  }, 500);
+  const workersInput = document.getElementById("workers");
+  workersInput.value = numWorkers;
+  workersInput.oninput = debounce(e => {
+    let parsedValue = Number(e.target.value);
+    if (isNaN(parsedValue) || parsedValue < 1) {
+      parsedValue = numCores;
+    }
+    workersInput.value = parsedValue;
+    numWorkers = parsedValue;
+    refreshMap(map);
   }, 500);
   const smoothingInput = document.getElementById("smoothing");
   smoothingInput.checked = true;
   smoothingInput.onclick = e => {
     isSmoothed = e.target.checked;
-    resetWorkers().then(() => {
-      refreshMap(map);
-    })
+    refreshMap(map);
   };
+  document.getElementById("refresh").onclick = () => refreshMap(map);
 }
 
 resetWorkers().then(() => handleInputs(createMap()));
