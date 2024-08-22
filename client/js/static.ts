9cb631e235ff15d3c851fc5965e409a67f86cdc1
Ross Hill <ross@rosshill.ca>
2024-08-21 22:40:05 -0400

    General cleanup

--- client/js/static.ts
+++ client/js/static.ts
@@ -10,7 +10,6 @@ import "../static/android-chrome-144x144.png";
 import "../static/favicon-32x32.png";
 import "../static/favicon-16x16.png";
 import "../static/site.webmanifest";
-import "../static/safari-pinned-tab.svg";
 import "../static/browserconfig.xml";
 import "../static/sitemap.xml";
 import "../static/favicon.ico";
