8089d74956877a7395dc25852d73d69888c4a59f
Ross Hill <ross@rosshill.ca>
2025-01-18 21:02:15 -0500

    Improve icons

--- client/js/static.ts
+++ client/js/static.ts
@@ -6,13 +6,12 @@ import "../node_modules/leaflet/dist/leaflet.css";
 // SEO and images
 import "../static/site-image.png";
 import "../static/apple-touch-icon.png";
-import "../static/android-chrome-144x144.png";
-import "../static/favicon-32x32.png";
-import "../static/favicon-16x16.png";
+import "../static/favicon-96x96.png";
+import "../static/icon512_maskable.png";
+import "../static/icon512_rounded.png";
+import "../static/icon192_maskable.png";
 import "../static/site.webmanifest";
-import "../static/browserconfig.xml";
 import "../static/sitemap.xml";
 import "../static/favicon.ico";
-import "../static/mstile-150x150.png";
 import "../static/blog-background.png";
 import "../netlify.toml";
